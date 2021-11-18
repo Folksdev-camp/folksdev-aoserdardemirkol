c72ff559c481bd27ba92ca1dad9122f83517e138
Ali Osman Serdar Demirkol <aoserdardemirkol@gmail.com>
2021-11-18 21:57:11 +0300

    swagger added

--- src/main/java/com/folksdev/blog/BlogApplication.java
+++ src/main/java/com/folksdev/blog/BlogApplication.java
@@ -1,7 +1,13 @@
 package com.folksdev.blog;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BlogApplication {
@@ -10,4 +16,20 @@ public class BlogApplication {
 		SpringApplication.run(BlogApplication.class, args);
 	}
 
+	@Bean
+	public OpenAPI customOpenAPI(
+			@Value("FolksDev Blog API") String title,
+			@Value("1.0") String version,
+			@Value("FolksDev SpringBoot Bootcamp süresince geliştirilen blog projesi") String description
+	) {
+		return new OpenAPI()
+				.info(new Info()
+						.title(title)
+						.version(version)
+						.description(description)
+						.contact(new Contact()
+								.name("Ali Osman Serdar Demirkol")
+								.url("https://github.com/aoserdardemirkol/" + "değiştir")
+								.email("aoserdardemirkol@gmail.com"))
+						.license(new License().name("Blog API Licence").url("https://github.com/aoserdardemirkol/" + "değiştir" + "/blob/master/LICENCE")));	}
 }
